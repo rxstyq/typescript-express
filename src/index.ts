ddf570ee1c15f75544ab7b63112d768815ff30f8
Panag06 <65036174+rxstyq@users.noreply.github.com>
2021-08-23 11:39:18 +0300

    ade kanw push

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import path from "path";
+const mongoose = require('mongoose');
+const configuration = require('./config.json');
+const app = express();
+const port = configuration.port;
+const routers = require('./router');
+const ejs = require('ejs');
+
+mongoose.connect(configuration.databaseURL, { 
+  useNewUrlParser: true, 
+  useFindAndModify: true, 
+  useUnifiedTopology: true }, async(error: any, info: any) =>{
+    if(error) throw error;
+    if(configuration.debug) console.log(`[DATABASE] Connected and listening at ${configuration.databaseURL}`);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use((req, res, next) => {
+  if(configuration.debug) console.log(`User requested at endpoint ${req.path ? `http://localhost:${port}${req.path}` : `http://localhost:${port}`}`)
+  next();
+})
+
+app.use(routers);
+
+app.listen(port, () => {
+  if(configuration.debug) console.log(`[ Server ] Application listening at http://localhost:${port}`)
+});
---
+++ src/router.ts
@@ -0,0 +1,43 @@
+import express from "express";
+const app = express();
+const mongoose = require('mongoose');
+const configuration = require('./config.json');
+const port = configuration.port;
+
+const model = mongoose.model("users", new mongoose.Schema({
+     Users: {
+      type: String,
+      required: true,
+    }
+}))
+
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  
+    res.status(200).json({ code: res.statusCode });
+  });
+
+  app.get('/users', async(req: express.Request, res: express.Response) => {
+    const users = await model.find({}).lean();
+    const array: any = [];
+    users.forEach((object: any) => {
+      array.push(object)
+    })
+    res.json({ users: array });
+  });
+
+  app.get('/users/:id', async(req: express.Request, res: express.Response) => {
+    if(req.params.id){
+      const userId = req.params.id.trim();
+      const users = await model.findOne({ id: userId }).lean();
+      if(!users) return res.send(`User with id: ${userId} does not exist try searching users between 1 - 3`);
+      res.send(users);
+    }
+  });
+  app.get('404', async(req, res) => {
+    res.status(404).render(`404page`);
+    })
+app.get('*', async(req, res) => {
+res.status(404).render(`404page`);
+})
+module.exports = app;
