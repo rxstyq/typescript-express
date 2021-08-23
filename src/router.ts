0e9630bd3d598f5d01cf8b19ee1c86dd29c8fa13
rxsty <65036174+rxstyq@users.noreply.github.com>
2021-08-23 11:47:09 +0300

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -15,29 +15,34 @@ const model = mongoose.model("users", new mongoose.Schema({
 app.get('/', (req: express.Request, res: express.Response) => {
   
     res.status(200).json({ code: res.statusCode });
-  });
+});
 
-  app.get('/users', async(req: express.Request, res: express.Response) => {
+app.get('/users', async(req: express.Request, res: express.Response) => {
     const users = await model.find({}).lean();
     const array: any = [];
     users.forEach((object: any) => {
       array.push(object)
     })
     res.json({ users: array });
-  });
+});
 
-  app.get('/users/:id', async(req: express.Request, res: express.Response) => {
-    if(req.params.id){
+app.get('/users/:id', async(req: express.Request, res: express.Response) => {
+  if(req.params.id){
       const userId = req.params.id.trim();
       const users = await model.findOne({ id: userId }).lean();
       if(!users) return res.send(`User with id: ${userId} does not exist try searching users between 1 - 3`);
       res.send(users);
-    }
-  });
-  app.get('404', async(req, res) => {
+          }
+});
+
+app.get('404', async(req, res) => {
+     
     res.status(404).render(`404page`);
-    })
-app.get('*', async(req, res) => {
-res.status(404).render(`404page`);
 })
+
+app.get('*', async(req, res) => {
+     
+res.status(404).render(`404page`);  
+})
+
 module.exports = app;
